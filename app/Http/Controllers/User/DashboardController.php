db6e736eecc0e25c6b5f4f3a8e03fda6ebaa79eb
Taraq Rahman <taraqr9@gmail.com>
2023-11-23 10:13:35 +0600

    WIP User side bootstraping

---
+++ app/Http/Controllers/User/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(): View
+    {
+        view()->share('page', config('app.nav')['dashboard']);
+
+        return view('user.dashboard');
+    }
+
+}
