f92e30b055cfdd5a5b1015324f3575e5c8775a82
MD.Taraq Rahman <taraq.rahman@bol-online.com>
2025-08-13 15:49:10 +0600

    Added view for setting

---
+++ app/Filament/Resources/SettingResource/Pages/ViewSetting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SettingResource\Pages;
+
+use App\Filament\Resources\SettingResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewSetting extends ViewRecord
+{
+    protected static string $resource = SettingResource::class;
+}
