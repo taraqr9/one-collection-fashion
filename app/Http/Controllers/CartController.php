7ee1151d8ebcf8318cd56996d678d8f5a8e8e7f4
MD.Taraq Rahman <taraq.rahman@bol-online.com>
2025-08-24 10:05:02 +0600

    update cart validation

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class CartController extends Controller
 {
-    public function index(): View
+    public function index(): View | RedirectResponse
     {
         $products = auth()->user()->carts()->get();
+
+        if( $products->isEmpty()) {
+            return redirect()->back()->with('error', 'Cart is empty');
+        }
 
         return view('user.cart.index', compact('products'));
     }
